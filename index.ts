526e904038bb9572b8a776fedab7b743e27b8f73
Rfk <rafackyou@gmail.com>
2020-04-14 11:37:41 -0700

    updates package.json

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as DataBaseModel } from './src/db';
+export { default as DatabaseStack } from './src/stack';
+export { default as ResolverModel } from './src/stack';
+export { default as RslvGenerator } from './src/gen'
