6535b2ed73d48f46bb0db1f2c9f6f2eca61eae87
Rfk <rafackyou@gmail.com>
2020-07-17 14:08:17 -0700

    adds lodash

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@
 export { default as DataBaseModel } from './db';
 export { default as DatabaseStack } from './stack';
 export { ContextBuilder , StackBuilder } from './builder';
+export { GroupModels } from './gen'
